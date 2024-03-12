5efdadfc377132836c20e55fd377031fe2cf4471
HARIS AHMED <136917000+harisahmedjs@users.noreply.github.com>
2024-03-13 00:16:39 +0500

    working

--- index.mjs
+++ index.mjs
@@ -1,7 +1,10 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT
+dotenv.config();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -10,3 +13,16 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log('DATABASE CONNECTED');
+  } catch (error) {
+    console.log(error);
+  }
+}
+connectDB().then(() => {
+  app.listen(process.env.PORT)
+}).catch((err) => {
+  console.log(err)
+})
